5e4bdf0a1c41c13f834b1d26c4852774f34d259c
pratik-codeX <pratikraut.codex@gmail.com>
2026-07-22 11:38:41 +0530

    feat(angular): add component practice projects

---
+++ Angular/EventBindind/src/app/demo/demo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  imports: [],
+  templateUrl: './demo.html',
+  styleUrl: './demo.css',
+})
+export class Demo {}
