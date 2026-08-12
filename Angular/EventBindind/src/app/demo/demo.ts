5fdd382c0d0da0c5ebe51aa00b0074a808f08902
pratik-codex <pratikraut.codex@gmail.com>
2026-08-12 23:54:11 +0530

    -

--- Angular/EventBindind/src/app/demo/demo.ts
+++ Angular/EventBindind/src/app/demo/demo.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Console } from 'console';
+import { ConnectableObservable } from 'rxjs';
 
 @Component({
   selector: 'app-demo',
@@ -6,4 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './demo.html',
   styleUrl: './demo.css',
 })
-export class Demo {}
+export class Demo 
+{
+  fun():number
+  {
+    var No1 = 10;
+    var No2 = 20;
+
+    return No1+No2;
+  }
+
+  public name : string = " "
+
+  public MarvellousEvent()
+  {
+    console.log("Marvellous Button Clicked");
+  }
+
+   public InfosystemsEvent()
+    {
+      this.name = "Infosystems button gets clicked";
+    }
+
+    public AngularEvent(Value:any)
+    {
+      console.log(Value);
+    }
+}
